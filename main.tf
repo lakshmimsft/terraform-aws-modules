0651aaf9b2b866b34beec0e6db602f625edd3f96
Lakshmi Javadekar <103459615+lakshmimsft@users.noreply.github.com>
2024-02-11 21:11:05 -0800

    updating module

--- main.tf
+++ main.tf
@@ -11,33 +11,79 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_s3_bucket" "bucket" {
+resource "aws_s3_bucket" "radbucket" {
   bucket = var.bucket_name
-  acl    = "private"
-
-  tags = {
-    Environment = var.context.environment.name
-    Applications = var.context.application.name
-    Resource = var.context.resource.name
-  }
 }
 
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.bucket.id
+resource "aws_s3_bucket_cors_configuration" "radbucket" {
+  bucket = aws_s3_bucket.radbucket.id  
 
-  policy = jsonencode({
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }  
+}
+
+resource "aws_s3_bucket_acl" "radbucket" {
+    bucket = aws_s3_bucket.radbucket.id
+    acl    = "public-read"
+    depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.radbucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+  depends_on = [aws_s3_bucket_public_access_block.example]
+}
+
+resource "aws_iam_user" "bucket_name" {
+  name = "prod-media-bucket"
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.radbucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "bucket_name" {
+    bucket = aws_s3_bucket.radbucket.id
+    policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
         Principal = "*"
         Action = [
-          "s3:GetObject"
+          "s3:*",
         ]
+        Effect = "Allow"
         Resource = [
-          "${aws_s3_bucket.bucket.arn}/*"
+          "arn:aws:s3:::${var.bucket_name}",
+          "arn:aws:s3:::${var.bucket_name}/*"
         ]
-      }
+      },
+      {
+        Sid = "PublicReadGetObject"
+        Principal = "*"
+        Action = [
+          "s3:GetObject",
+        ]
+        Effect   = "Allow"
+        Resource = [
+          "arn:aws:s3:::${var.bucket_name}",
+          "arn:aws:s3:::${var.bucket_name}/*"
+        ]
+      },
     ]
   })
+  
+  depends_on = [aws_s3_bucket_public_access_block.example]
 }
